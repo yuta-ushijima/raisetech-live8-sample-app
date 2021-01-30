e34e032759d88ca9fd572e2433e65a7cd06f2757
Yuta_Ushizima <contact@yuta-u.com>
2021-01-30 12:56:48 +0900

    feat: Implement layout by bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+// jQueryとBootstapのJSを使えるように
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: "popper.js",
+  })
+);
+
+module.exports = environment;
