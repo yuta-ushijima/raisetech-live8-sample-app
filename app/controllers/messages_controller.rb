de213cbda1396ddd3230156c2901c4fb65335c55
Yuta_Ushizima <contact@yuta-u.com>
2023-04-22 14:09:33 +0900

    feat: Implement MessagesController action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'messages', message: @message.content
+      head :ok
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
