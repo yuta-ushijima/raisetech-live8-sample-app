88f07f57d893f55ea931747c031cb42d7a644699
Yuta_Ushizima <contact@yuta-u.com>
2023-04-23 12:07:33 +0900

    feat: delete unnecessary fiels and refactoring

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,5 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
-    @message = Message.new
-  end
-
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-      head :ok
-    else
-      render :index
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content)
   end
 end
