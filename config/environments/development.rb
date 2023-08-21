3a916ce64cb590479c42cd8285e69dd4266a3c04
dende-h <dende-h@ymail.ne.jp>
2023-08-21 06:03:59 +0000

    bugfix:Blocked hostエラーの解消とRails Asset Pipelineエラーの解消

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "52b5155bbb8d40c0a94084e5b8b24625.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
